db1d900bb220d8b9bb44d95dff680c5f0beb1ae2
MatheusDSilver <matheusdasilvaalvesm@gmail.com>
2025-07-10 15:23:00 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿var p1 = Pescar();
+var c1 = MascarCana();
+
+await p1;
+await c1;
+
+Console.WriteLine($"Conseguiu pegar um peixe de {p1.Result} cm depois de mascar {c1.Result} canas");
+
+async Task<int> Pescar()
+{
+    for(int i = 0; i < 5; i++)
+    {
+        await Task.Delay(500);
+        Console.WriteLine($"Perdeu {i} minhocas tentando pegar o peixe \n");
+    }
+
+    return new Peixe().TamanhoDoPeixe();
+}
+
+async Task<int> MascarCana()
+{
+    int canas = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        await Task.Delay(500);
+        Console.WriteLine($"Mascou {i} canas \n");
+        canas = i;
+    }
+    return canas;
+}
+
+public class Peixe
+{
+    Random randNum = new Random();
+    private int _tamanho;
+    public Peixe()
+    {
+        _tamanho = (int)randNum.NextInt64(1, 150);
+    }
+    public int TamanhoDoPeixe()
+    {
+        return _tamanho;
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# CanasEPeixes-Async
+
+Aprendendo async pescando e mascando cana em um projeto Console Application
+
+## Objetivo
+
+Simular mascar cana e pescar ao mesmo tempo usando programação assíncrona
+
+## Linguagem e recursos
+
+- C#
+- Dotnet
+- Task / async-await
